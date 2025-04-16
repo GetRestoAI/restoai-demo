30e4fa76073762b2bdb292ac2c3ee9d322a04589
GetRestoAI <richard@madebyley.nl>
2025-04-16 10:45:14 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'Arial', padding: '2rem' }}>
+      <h1>Welkom bij de Resto.ai Demo</h1>
+      <p>Bekijk hier de verschillende modules:</p>
+
+      <Link href="/tekorten">
+        <button style={{ marginTop: '1rem', marginRight: '1rem', padding: '0.5rem 1rem' }}>
+          Bekijk tekorten
+        </button>
+      </Link>
+
+      <Link href="/rooster">
+        <button style={{ marginTop: '1rem', padding: '0.5rem 1rem' }}>
+          Roostermodule openen
+        </button>
+      </Link>
+    </main>
+  )
+}
